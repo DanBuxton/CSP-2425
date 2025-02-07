f46b34ecf7fbc41a4c4c0e5021f5757e2cabacb2
Buxton, Dan <dan.buxton@ntu.ac.uk>
2025-02-07 10:44:43 +0000

    Added a sample practice test

---
+++ Lab4-PracticeTest/Lab4-PracticeTest.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+const int NUM_LAPTOPS = 5;
+
+double price[NUM_LAPTOPS] = { 350.0, 750.0, 1200.0, 499.0, 999.0 };
+int serial[NUM_LAPTOPS] = { 1234, 5678, 9101, 1121, 3141 };
+
+int main()
+{
+
+}
